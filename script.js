51d8097c97064ef1a85f0a8a2c94ac9044f28ab7
CorneliusC7 <84184180+CorneliusC7@users.noreply.github.com>
2022-03-09 23:36:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  }
+  
 function newroom(){
     open("room.html", "_self")
 }
 function join(code){
-    open("join.html?room="+code, "_self")
+    if(code == "" || code.length < 6){
+        alert("Please enter a room code");
+    }else{
+        open("join.html?room="+code, "_self");
+    }
 }
 function joinroom(){
     join(document.getElementById("join").value);
 }
 function create(roomName, roomCode) {
-    firebase.database().ref('room/' + roomCode).set({
-      roomName: roomName,
-      roomCode: roomCode,
-      chat: ["Welcome to the room!"]
-    });
-    show();
-    setTimeout(function(){
-        join(roomCode);
-    },2000)
+    if(roomName == "" || roomCode == "" || isNumeric(roomCode) == false || roomCode.length < 6){
+        alert("Please enter a room name and room code");
+    }
+    else{
+        firebase.database().ref('room/' + roomCode).set({
+            roomName: roomName,
+            roomCode: roomCode,
+            chat: ["Welcome to the room!"]
+        });
+        localStorage.setItem("room", roomName);
+        show();
+        setTimeout(function(){
+            join(roomCode);
+        },2000)
+    }
 }
 function createroom(){
     var roomName = document.getElementById("room_name").value;
@@ -28,4 +44,7 @@ function hides(){
 }
 function show(){
     document.getElementById("hides").style.display = "block";
+}
+function source(){
+    open("https://github.com/CorneliusC7/p2pchat")
 }
